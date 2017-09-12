458df66e70490f586e341aaee19c3b4bc7b923c2
Thang Tran <thangtran070495@gmail.com>
2017-09-12 15:55:00 +0700

    update home en

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -8,4 +8,30 @@ return [
     'web'=>'Web Templates',
     'web_content' => 'Mua template chỉ với 1.500.000 VNĐ',
 
+    'ttnt'=>'Artificial Intelligence',
+    'xdweb'=>'Web and Mobile apps Development for impressive achievements',
+    'ptthuonghieu'=>'Artificial Inteligence, and Smart Solutions is what we are apllying in building your business brands.',
+    'slogan' => 'Artificial intelligence - The magic for your brand.',
+    'duannoibat' => 'OUR FEATURED PROJECTS';
+    'thanhcong' => 'Achievements we get are the measure of our success today.',
+    'sansang' => 'Ready for your project discussion? Contact now…?',
+    'lienhengay' => 'Contact us',
+    'wephucvu' => 'We offer our sevices to all industries. .',
+    'wedungdau' => 'We stay on top of industries in Artificial Intelligence',
+    'doanhnghiep' => 'Enterprise',
+    'giaoduc' => 'Education',
+    'philoinhuan' => 'Nonprofit',
+    'xemthem' => 'Read more',
+    '3buoc' => '3 steps approach to developing an online brand ',
+    'tintuc' => 'News',
+    'docthem' => 'Read more',
+    'allpress' => 'ALL PRESS',
+    'allblog' => 'ALL BLOG',
+    'thaoluan' => 'Ready for your project discussion?',
+    'dangky' => 'Subcribe for more latest tech trends.',
+    'email' => 'Enter email',
+    'register' => 'REGISTER',
+    'diachi' => 'ADRESS'
+    'thamgia' => 'Join HBBS with social network',
+
 ];
