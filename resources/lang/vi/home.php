afde05c10a802b16c47cbf087d542d1d063a0044
Thang Tran <thangtran070495@gmail.com>
2017-09-12 15:55:32 +0700

    update home vn

--- resources/lang/vi/home.php
+++ resources/lang/vi/home.php
@@ -7,4 +7,32 @@ return [
     'contact'=>'Liên hệ',
     'web'=>'Web Templates',
     'web_content' => 'Mua template chỉ với 1.500.000 VNĐ',
+
+    'ttnt'=>'Trí tuệ nhân tạo',
+    'xdweb'=>'Xây dựng Web và Mobile apps với kết quả ấn tượng.',
+    'ptthuonghieu'=>'Chúng tôi phát triển thương hiệu cùng với việc xây dựng các giải pháp thông minh và ứng dụng trí tuệ nhân tạo.',
+    'slogan' => 'Chúng tôi xây dựng thương hiệu dựa trên trí tuệ nhân tạo.',
+    'duannoibat' => 'DỰ ÁN NỔI BẬT';
+    'thanhcong' => 'Thành công hôm nay của chúng tôi được đo lường bằng những kết quả đã đạt được.',
+    'sansang' => 'Sẵn sàng để thảo luận về dự án của bạn?',
+    'lienhengay' => 'Liên Hệ Ngay',
+    'wephucvu' => 'Chúng tôi phục vụ tất cả các ngành công nghiệp',
+    'wedungdau' => 'Chúng tôi đứng đầu trong ngành công nghiệp về trí tuệ nhân tạo.',
+    'doanhnghiep' => 'Doanh Nghiệp',
+    'giaoduc' => 'Giáo Dục',
+    'philoinhuan' => 'Phi Lợi Nhuận',
+    'xemthem' => 'Xem Thêm',
+    '3buoc' => '3 bước tiếp cận để phát triển một thương hiệu trực tuyến',
+    'tintuc' => 'Tin Tức',
+    'docthem' => 'Đọc Thêm',
+    'allpress' => 'TẤT CẢ PRESS',
+    'allblog' => 'TẤT CẢ BLOG',
+    'thaoluan' => 'Sẵn sàng để thảo luận về dự án của bạn?',
+    'dangky' => 'Đăng ký để nhận những xu hướng công nghệ mới nhất.',
+    'email' => 'Nhập email',
+    'register' => 'ĐĂNG KÝ',
+    'diachi' => 'ĐỊA CHỈ'
+    'thamgia' => 'Tham Gia Mạng Xã Hội Với HBBS',
+    
+
 ];
