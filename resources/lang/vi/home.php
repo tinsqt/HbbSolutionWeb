3b75bdf11602b04cc66dfdb5caa1b213ba287ae1
Thang Tran <thangtran070495@gmail.com>
2017-09-13 11:09:36 +0700

    update home vi

--- resources/lang/vi/home.php
+++ resources/lang/vi/home.php
@@ -9,20 +9,20 @@ return [
     'web_content' => 'Mua template chỉ với 1.500.000 VNĐ',
 
     'ttnt'=>'Trí tuệ nhân tạo',
-    'xdweb'=>'Xây dựng Web và Mobile apps với kết quả ấn tượng.',
-    'ptthuonghieu'=>'Chúng tôi phát triển thương hiệu cùng với việc xây dựng các giải pháp thông minh và ứng dụng trí tuệ nhân tạo.',
-    'slogan' => 'Chúng tôi xây dựng thương hiệu dựa trên trí tuệ nhân tạo.',
-    'duannoibat' => 'DỰ ÁN NỔI BẬT';
+    'xdweb'=>'Xây dựng<br class="show-in-mobile"> Web và Mobile apps <br> <br class="show-in-tablet show-in-mobile"> <strong>với kết quả ấn tượng.</strong>',
+    'ptthuonghieu'=>'Chúng tôi <strong>phát triển thương hiệu</strong> cùng với việc xây dựng các giải pháp thông minh và ứng dụng trí tuệ nhân tạo.',
+    'slogan' => 'Chúng tôi xây dựng thương hiệu <br> dựa trên trí tuệ nhân tạo.',
+    'duannoibat' => 'DỰ ÁN <br class="show-in-mobile">NỔI BẬT',
     'thanhcong' => 'Thành công hôm nay của chúng tôi được đo lường bằng những kết quả đã đạt được.',
-    'sansang' => 'Sẵn sàng để thảo luận về dự án của bạn?',
+    'sansang' => 'Sẵn sàng <span>để</span> thảo luận về <br class="show-in-tablet">dự án của bạn?',
     'lienhengay' => 'Liên Hệ Ngay',
-    'wephucvu' => 'Chúng tôi phục vụ tất cả các ngành công nghiệp',
-    'wedungdau' => 'Chúng tôi đứng đầu trong ngành công nghiệp về trí tuệ nhân tạo.',
+    'wephucvu' => 'Chúng tôi phục vụ <br>tất cả các ngành công nghiệp<br class="show-in-mobile">',
+    'wedungdau' => 'Chúng tôi đứng đầu trong ngành công nghiệp <br class="show-in-mobile"> về trí tuệ nhân tạo.',
     'doanhnghiep' => 'Doanh Nghiệp',
     'giaoduc' => 'Giáo Dục',
     'philoinhuan' => 'Phi Lợi Nhuận',
     'xemthem' => 'Xem Thêm',
-    '3buoc' => '3 bước tiếp cận để phát triển một thương hiệu trực tuyến',
+    '3buoc' => '<span>3 bước tiếp cận để </span> <br/>phát triển một thương hiệu trực tuyến',
     'tintuc' => 'Tin Tức',
     'docthem' => 'Đọc Thêm',
     'allpress' => 'TẤT CẢ PRESS',
@@ -31,8 +31,43 @@ return [
     'dangky' => 'Đăng ký để nhận những xu hướng công nghệ mới nhất.',
     'email' => 'Nhập email',
     'register' => 'ĐĂNG KÝ',
-    'diachi' => 'ĐỊA CHỈ'
+    'diachi' => 'ĐỊA CHỈ',
     'thamgia' => 'Tham Gia Mạng Xã Hội Với HBBS',
-    
+    'thank' => 'Cảm ơn!',
+    'gptm' => 'Giải Pháp Thông Minh',
+    'menuxd' => 'Xây Dựng Web',
+    'xdapp' => 'Xây Dựng Mobile Apps',
+
+    // Sản Phẩm
+    'beforafter' => 'TRƯỚC &amp; <span>SAU</span>',
+    'before' => 'Trước',
+    'after' => 'Sau',
+
+    // Dịch Vụ
+    'ttnt2' => 'Trí Tuệ<br> Nhân Tạo',
+    'xdweb2' => 'Xây Dựng <br />Website',
+    'xdapp2' => 'Xây Dựng <br /> Mobile Apps',
+    'xdth' => '<strong>Xây Dựng Thương Hiệu Với<br> AI Dựa Trên Giải Pháp Thông Minh</strong>',
+    'alldichvu' => 'Tất cả các dịch vụ giúp bạn <br><strong>phát triển thương hiệu trực tuyến</strong>',
+
+    // Đối tác
+    'clients' => 'CÁC ĐỐI TÁC',
+    'tinnhiem' => 'Chúng tôi được tín nhiệm bởi các công ty có quy mô lớn trên thế giới.',
+    'cohoi' => 'HBB Solutions đã có cơ hội làm việc với một số tên tuổi lớn trong giới doanh nghiệp cũng như nhiều công ty khởi nghiệp tiềm năng.',
+
+
+    // Liên Hệ
+    'lienheus' => '<span>Liên Hệ</span> Với Chúng Tôi',
+    'hoten' => 'Họ và Tên',
+    'sdt' => 'Số Điện Thoại',
+    'company' => 'Công ty',
+    'noidung' => 'Nội Dung:',
+    'send' => 'GỬI',
+    'hcmc' => 'Thành Phố HCM',
+    'infoadr' => '244 Cống Quỳnh,<br> Lầu 3, Tòa nhà JABELS, <br>Phường Phạm Ngũ Lão, Quận 1',
+    'llus' => 'Liên lạc với chúng tôi',
+    'tuyendung' => 'Chúng tôi đang tuyển dụng!',
+    'phongvan' => 'Tham gia phỏng vấn để có cơ hội việc làm tốt nhất.',
+    'chitiet' => 'XEM CHI TIẾT'
 
 ];
