b2aefdace8ef590e1b19511eabce59e0630863ff
Thang Tran <thangtran070495@gmail.com>
2017-09-13 11:08:57 +0700

    update home en

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -6,23 +6,23 @@ return [
     'about'=>'About us',
     'contact'=>'Contact',
     'web'=>'Web Templates',
-    'web_content' => 'Mua template chỉ với 1.500.000 VNĐ',
+    'web_content' => 'Buy template just 1.500.000 VNĐ',
 
     'ttnt'=>'Artificial Intelligence',
-    'xdweb'=>'Web and Mobile apps Development for impressive achievements',
-    'ptthuonghieu'=>'Artificial Inteligence, and Smart Solutions is what we are apllying in building your business brands.',
+    'xdweb'=>'Web and Mobile apps Development <br class="show-in-tablet show-in-mobile"> <strong> for impressive achievements.</strong>',
+    'ptthuonghieu'=>'Artificial Inteligence, and <strong> Smart Solutions</strong> is what we are apllying in building your business brands.',
     'slogan' => 'Artificial intelligence - The magic for your brand.',
-    'duannoibat' => 'OUR FEATURED PROJECTS';
+    'duannoibat' => 'OUR FEATURED <br class="show-in-mobile"> PROJECTS',
     'thanhcong' => 'Achievements we get are the measure of our success today.',
-    'sansang' => 'Ready for your project discussion? Contact now…?',
+    'sansang' => 'Ready <span>for</span> your project discussion?<br class="show-in-tablet"> Contact now…?',
     'lienhengay' => 'Contact us',
-    'wephucvu' => 'We offer our sevices to all industries. .',
-    'wedungdau' => 'We stay on top of industries in Artificial Intelligence',
+    'wephucvu' => 'We offer <br>our sevices to all industries.<br class="show-in-mobile">',
+    'wedungdau' => 'We stay on top of industries in <br class="show-in-mobile">Artificial Intelligence',
     'doanhnghiep' => 'Enterprise',
     'giaoduc' => 'Education',
     'philoinhuan' => 'Nonprofit',
     'xemthem' => 'Read more',
-    '3buoc' => '3 steps approach to developing an online brand ',
+    '3buoc' => '<span>3 steps approach</span> <br>to developing an online brand ',
     'tintuc' => 'News',
     'docthem' => 'Read more',
     'allpress' => 'ALL PRESS',
@@ -31,7 +31,44 @@ return [
     'dangky' => 'Subcribe for more latest tech trends.',
     'email' => 'Enter email',
     'register' => 'REGISTER',
-    'diachi' => 'ADRESS'
+    'diachi' => 'ADRESS',
     'thamgia' => 'Join HBBS with social network',
+    'thank' => 'Thank You!',
+    'gptm' => 'Smart Solutions',
+    'menuxd' => 'Build Web',
+    'xdapp' => 'Build Mobile Apps',
+
+    // Sản Phẩm
+    'beforafter' => 'BEFORE & AFTER',
+    'befor' => 'Before',
+    'after' => 'After',
+
+    // Dịch Vụ
+    'ttnt2' => 'Artificial<br> Intelligence',
+    'xdweb2' => 'Build <br />Website',
+    'xdapp2' => 'Build <br /> Mobile Apps',
+    'xdth' => '<strong>Building Brands with<br> AI Based Smart Solutions</strong>',
+    'alldichvu' => 'All our services help you <br><strong>develop your online brand.</strong>',
+
+    // Đối tác
+    'clients' => 'CLIENTS',
+    'tinnhiem' => 'HBBS-a place for trust giving.',
+    'cohoi' => 'HBB Solutions has had the opportunities to work with a number of huge enterprises as well as potential startups.',
+
+
+    // Liên Hệ
+    'lienheus' => '<span>Contact</span> Us',
+    'hoten' => 'Fullname',
+    'sdt' => 'Phone number',
+    'company' => 'Company',
+    'noidung' => 'Content',
+    'send' => 'SEND',
+    'hcmc' => 'Ho Chi Minh City',
+    'infoadr' => '244 Cong Quynh,<br> Floor3rd, JABES Building, <br>Pham Ngu Lao, District 1',
+    'llus' => 'Contact us',
+    'tuyendung' => 'We are hiring!',
+    'phongvan' => 'Join in the interview for the best job apportunies.',
+    'chitiet' => 'DETAIL'
+
 
 ];
